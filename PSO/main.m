c566097df6f0d0aedd2aabc820218328a49a70fe
Ali <114668273+assi99@users.noreply.github.com>
2025-09-01 12:48:58 +0200

    Update main.m

--- PSO/main.m
+++ PSO/main.m
@@ -1,35 +1,35 @@
 clear; clc; close all;
 
-% Load STL mesh and geometric features
+% STL mesh and geometric features
 [mesh, features] = load_stl_features('solution1STLfile.stl');
 
-% Run Particle Swarm Optimization (with mutation + Pareto tracking)
+% Run PSO (with mutation + pareto tracking)
 [best_transform, all_final_particles, positionHistory, pareto_log] = pso(features, mesh);
 
-% Visualize the final optimized geometry
+% Visualize final optimized geometry
 visualize_result(mesh, best_transform);
 
-% Volume vs. Area scatter of all final particle geometries
+% Volume vs. Area scatter of all final PSO geometries
 plot_volume_area_tradeoff(mesh, features, all_final_particles);
 
-% Plot how volume and area evolved over time
+% Ploting how volume and area evolve over time
 plot_volume_area_over_time(positionHistory, mesh, features);
 
 % Animate how one parameter affects performance
 animate_sweep(mesh, features, best_transform, 4);  % 4 = pitch
 
-% Overlay all particle geometries on STL mesh
+% Overlayig all particle geometries on STL mesh
 plot_swarm_on_stl(mesh, features, all_final_particles);
 
-% Plot parameter evolution
+% Ploting parameter evolution
 plot_parameter_evolution(positionHistory);
 
-% Plot fitness isocurves (scaleZ vs pitch)
+% Ploting fitness isocurves (scaleZ vs pitch)
 plot_fitness_isocurves(mesh, features);
 
 % Save data if needed
 save('pso_results.mat', 'best_transform', 'positionHistory', 'pareto_log');
 
-color_stl_by_metric(mesh, features, best_transform, "inertia"); % or "area", "symmetry"
+color_stl_by_metric(mesh, features, best_transform, "inertia"); 
 
 run_multi_pso(mesh, features, 5);
