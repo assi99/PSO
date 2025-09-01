c0955e2129913557db584407333af2b3195141c7
Ali <114668273+assi99@users.noreply.github.com>
2025-09-01 12:52:10 +0200

    Update run_multi_pso.m

--- PSO/run_multi_pso.m
+++ PSO/run_multi_pso.m
@@ -1,7 +1,7 @@
 function all_runs = run_multi_pso(mesh, features, num_runs)
     all_runs = cell(num_runs, 1);
     for i = 1:num_runs
-        fprintf('\n🔥 Running PSO #%d of %d\n', i, num_runs);
+        fprintf('\n Running PSO %d of %d\n', i, num_runs);
         [best, particles, history, pareto] = pso(features, mesh);
         all_runs{i}.best = best;
         all_runs{i}.particles = particles;
